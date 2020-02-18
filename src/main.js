8e714f3195efb743475021c926b20ef6d4b2292b
wanghao <wanghao@ninghao.net>
2020-02-18 12:55:40 +0800

    创建 Web 服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-const greeting = '你好！';
-console.log(greeting);
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('hello ~');
+  response.end();
+});
+
+server.listen(3000, () => {
+  console.log('🚀 服务已启动！');
+});
