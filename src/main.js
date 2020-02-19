84f1074dd2a2ef39f0a1db6269d485a8c797e86f
wanghao <wanghao@ninghao.net>
2020-02-19 12:43:05 +0800

    请求的地址

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  console.log(request.headers['user-agent']);
+  switch (request.url) {
+    case '/':
+      response.write('hello ~');
+      break;
+    case '/posts':
+      response.write('posts');
+      break;
+    case '/signup':
+      response.write('signup');
+      break;
+    default:
+      response.writeHead(404);
+      response.write('404');
+      break;
+  }
 
-  response.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-  response.write(`<input />`);
   response.end();
 });
 
