9af7df8bcd6122e43e1197776ac9bf617c445dac
wanghao <wanghao@ninghao.net>
2020-02-19 11:23:39 +0800

    请求：Request

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+  console.log(request.headers['user-agent']);
+
   response.write('hello ~');
   response.end();
 });
