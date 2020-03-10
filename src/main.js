7834ae5e397c9acd1917e381a5ba04ef021241ca
wanghao <wanghao@ninghao.net>
2020-03-10 12:03:34 +0800

    创建 Web 服务器<Express.js>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,7 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const data = {
-    id: 1,
-    title: '关山月',
-    content: '明月出天山，苍茫云海间'
-  }
-
-  const jsonData = JSON.stringify(data);
-
-  response.writeHead(200, {
-    'Content-Type': 'application/json; charset=utf-8'
-  });
-
-  response.write(jsonData);
-
-  response.end();
-});
-
-server.listen(3000, () => {
+app.listen(port, () => {
   console.log('🚀 服务已启动！');
 });
