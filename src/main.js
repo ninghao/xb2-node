db0051e724d6f411a977272f05b4e1417eafb4a5
wanghao <wanghao@ninghao.net>
2020-03-10 12:07:48 +0800

    准备演示数据

--- src/main.js
+++ src/main.js
@@ -9,3 +9,21 @@ app.listen(port, () => {
 app.get('/', (request, response) => {
   response.send('你好');
 });
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝'
+  }
+];
