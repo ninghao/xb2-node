2452fc871a2e64af50afccd74ddd810c8c7c1c7e
wanghao <wanghao@ninghao.net>
2020-03-19 10:59:39 +0800

    把应用的配置放在环境变量里

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/**
+ * 应用配置
+ */
+export const { APP_PORT } = process.env;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import app from './app';
+import { APP_PORT } from './app/app.config';
 
-app.listen(3000, () => {
+app.listen(APP_PORT, () => {
   console.log('🚀 服务已启动！');
 });
