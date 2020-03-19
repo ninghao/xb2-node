c239ca8eb3d9552473dfb53497fb0ad4f69aaa6b
wanghao <wanghao@ninghao.net>
2020-03-19 11:21:36 +0800

    创建默认的异常处理器

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -11,3 +11,27 @@ export const requestUrl = (
   console.log(request.url);
   next();
 };
+
+/**
+ * 默认异常处理器
+ */
+export const defaultErrorHandler = (
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  let statusCode: number, message: string;
+
+  /**
+   * 处理异常
+   */
+  switch (error.message) {
+    default:
+      statusCode = 500;
+      message = '服务暂时出了点问题 ~~ 🌴';
+      break;
+  }
+
+  response.status(statusCode).send({ message });
+};
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import postRouter from '../post/post.router';
+import { defaultErrorHandler } from './app.middleware';
 
 /**
  * 创建应用
@@ -15,6 +16,11 @@ app.use(express.json());
  * 路由
  */
 app.use(postRouter);
+
+/**
+ * 默认异常处理器
+ */
+app.use(defaultErrorHandler);
 
 /**
  * 导出应用
