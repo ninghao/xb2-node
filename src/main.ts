0740f12d0a8c877a54e1c7afbd491418456a0c19
wanghao <wanghao@ninghao.net>
2020-03-25 15:01:28 +0800

    测试连接数据仓库

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import app from './app';
 import { APP_PORT } from './app/app.config';
+import { connection } from './app/database/mysql';
 
 app.listen(APP_PORT, () => {
   console.log('🚀 服务已启动！');
+});
+
+/**
+ * 测试使用数据服务连接
+ */
+connection.connect(error => {
+  if (error) {
+    console.log('👻 连接数据服务失败：', error.message);
+    return;
+  }
+
+  console.log('🚛 成功连接数据服务 ~~');
 });
