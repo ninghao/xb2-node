aba0e7a70e4ba67cc31364d0ff47cd147b619cb6
wanghao <wanghao@ninghao.net>
2020-03-26 14:10:06 +0800

    处理创建用户的错误

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -31,6 +31,14 @@ export const defaultErrorHandler = (
    * 处理异常
    */
   switch (error.message) {
+    case 'NAME_IS_REQUIRED':
+      statusCode = 400;
+      message = '请提供用户名';
+      break;
+    case 'PASSWORD_IS_REQUIRED':
+      statusCode = 400;
+      message = '请提供用户密码';
+      break;
     default:
       statusCode = 500;
       message = '服务暂时出了点问题 ~~ 🌴';
