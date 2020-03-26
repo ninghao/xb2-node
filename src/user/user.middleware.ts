b99a99beafb2af19505db8700aaad614e7d3c8a0
wanghao <wanghao@ninghao.net>
2020-03-26 14:06:54 +0800

    验证用户数据

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,2 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
+
+/**
+ * 验证用户数据
+ */
+export const validateUserData = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log('👮‍♂️ 验证用户数据');
+
+  // 准备数据
+  const { name, password } = request.body;
+
+  // 验证必填数据
+  if (!name) return next(new Error('NAME_IS_REQUIRED'));
+  if (!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+
+  // 下一步
+  next();
+};
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as userController from './user.controller';
+import { validateUserData } from './user.middleware';
 
 const router = express.Router();
 
 /**
  * 创建用户
  */
-router.post('/users', userController.store);
+router.post('/users', validateUserData, userController.store);
 
 /**
  * 导出路由
