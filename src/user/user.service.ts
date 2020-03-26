3396688af4ad1a68ccebef4ae0d3594680cecf86
wanghao <wanghao@ninghao.net>
2020-03-26 14:12:33 +0800

    定义按用户名查找用户的服务

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,3 +17,21 @@ export const createUser = async (user: UserModel) => {
   // 提供数据
   return data;
 };
+
+/**
+ * 按用户名查找用户
+ */
+export const getUserByName = async (name: string) => {
+  // 准备查询
+  const statement = `
+    SELECT id, name
+    FROM user
+    WHERE name = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, name);
+
+  // 提供数据
+  return data[0];
+};
