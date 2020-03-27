d6b1b64960d0f892dc0edb5b50fc52d409c474e6
wanghao <wanghao@ninghao.net>
2020-03-27 13:41:51 +0800

    用户登录

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,1 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+
+/**
+ * 用户登录
+ */
+export const login = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 准备数据
+  const { name, password } = request.body;
+
+  // 做出响应
+  response.send({ message: `欢迎回来，${name}` });
+}
--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -4,6 +4,11 @@ import * as authController from './auth.controller';
 const router = express.Router();
 
 /**
+ * 用户登录
+ */
+router.post('/login', authController.login);
+
+/**
  * 导出路由
  */
 export default router;
