6c7ca2a2c89600ed38909d765ddb24ead7facc01
wanghao <wanghao@ninghao.net>
2020-03-27 13:37:33 +0800

    身份验证模块

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import postRouter from '../post/post.router';
 import userRouter from '../user/user.router';
+import authRouter from '../auth/auth.router';
 import { defaultErrorHandler } from './app.middleware';
 
 /**
@@ -16,7 +17,7 @@ app.use(express.json());
 /**
  * 路由
  */
-app.use(postRouter, userRouter);
+app.use(postRouter, userRouter, authRouter);
 
 /**
  * 默认异常处理器
---
+++ src/auth/auth.router.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import * as authController from './auth.controller';
+
+const router = express.Router();
+
+/**
+ * 导出路由
+ */
+export default router;
