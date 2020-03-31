362f53e3ab4a8aa52d792b17049bdbdf6c8209e5
wanghao <wanghao@ninghao.net>
2020-03-31 20:21:52 +0800

    配置接口的访问控制

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as postController from './post.controller';
 import { requestUrl } from '../app/app.middleware';
-import { authGuard } from '../auth/auth.middleware';
+import { authGuard, accessControl } from '../auth/auth.middleware';
 
 const router = express.Router();
 
@@ -18,12 +18,22 @@ router.post('/posts', authGuard, postController.store);
 /**
  * 更新内容
  */
-router.patch('/posts/:postId', postController.update);
+router.patch(
+  '/posts/:postId',
+  authGuard,
+  accessControl({ possession: true }),
+  postController.update,
+);
 
 /**
  * 删除内容
  */
-router.delete('/posts/:postId', postController.destroy);
+router.delete(
+  '/posts/:postId',
+  authGuard,
+  accessControl({ possession: true }),
+  postController.destroy,
+);
 
 /**
  * 导出路由
