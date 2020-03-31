f91f92efd43c79b060b12c0556db0b85dfd6c863
wanghao <wanghao@ninghao.net>
2020-03-31 20:08:41 +0800

    定义检查用户是否拥有指定资源的功能

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { PRIVATE_KEY } from '../app/app.config';
+import { connection } from '../app/database/mysql';
 
 /**
  * 签发令牌
@@ -17,4 +18,33 @@ export const signToken = (options: SignTokenOptions) => {
 
   // 提供 JWT
   return token;
+};
+
+/**
+ * 检查用户是否拥有指定资源
+ */
+interface PossessOptions {
+  resourceId: number;
+  resourceType: string;
+  userId: number;
+}
+
+export const possess = async (options: PossessOptions) => {
+  // 准备选项
+  const { resourceId, resourceType, userId } = options;
+
+  // 准备查询
+  const statement = `
+    SELECT COUNT(${resourceType}.id) as count
+    FROM ${resourceType}
+    WHERE ${resourceType}.id = ? AND userId = ?
+  `;
+
+  // 检查拥有权
+  const [data] = await connection
+    .promise()
+    .query(statement, [resourceId, userId]);
+
+  // 提供检查结果
+  return data[0].count ? true : false;
 };
