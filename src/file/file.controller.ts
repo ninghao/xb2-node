648719035c8bfbe7c5fadeeb2f6604814793e04e
wanghao <wanghao@ninghao.net>
2020-04-03 17:06:25 +0800

    存储上传文件信息

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import _ from 'lodash';
+import { createFile } from './file.service';
 
 /**
  * 上传文件
@@ -8,6 +10,31 @@ export const store = async (
   response: Response,
   next: NextFunction,
 ) => {
-  console.log(request.file);
-  response.sendStatus(200);
+  // 当前用户
+  const { id: userId } = request.user;
+
+  // 所属内容
+  const { post: postId } = request.query;
+
+  // 文件信息
+  const fileInfo = _.pick(request.file, [
+    'originalname',
+    'mimetype',
+    'filename',
+    'size',
+  ]);
+
+  try {
+    // 保存文件信息
+    const data = await createFile({
+      ...fileInfo,
+      userId,
+      postId,
+    });
+
+    // 做出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
 };
