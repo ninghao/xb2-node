f9028a184ce700d5fa6f9349aa04b2e281732b45
wanghao <wanghao@ninghao.net>
2020-04-03 16:31:32 +0800

    定义上传文件的功能

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,1 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
+import multer from 'multer';
+
+/**
+ * 创建一个 Multer
+ */
+const fileUpload = multer({
+  dest: 'uploads/',
+});
+
+/**
+ * 文件拦截器
+ */
+export const fileInterceptor = fileUpload.single('file');
