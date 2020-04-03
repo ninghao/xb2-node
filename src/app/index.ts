a3f25e1fdcbf24975ae7794bac9916846eb054ab
wanghao <wanghao@ninghao.net>
2020-04-03 16:28:47 +0800

    文件模块

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import postRouter from '../post/post.router';
 import userRouter from '../user/user.router';
 import authRouter from '../auth/auth.router';
+import fileRouter from '../file/file.router';
 import { defaultErrorHandler } from './app.middleware';
 
 /**
@@ -17,7 +18,7 @@ app.use(express.json());
 /**
  * 路由
  */
-app.use(postRouter, userRouter, authRouter);
+app.use(postRouter, userRouter, authRouter, fileRouter);
 
 /**
  * 默认异常处理器
---
+++ src/file/file.service.ts
@@ -0,0 +1,1 @@
+import { connection } from '../app/database/mysql';
