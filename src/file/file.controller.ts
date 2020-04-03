c48d6c8fe1a9d06b66fd4c479d199e339b52698d
wanghao <wanghao@ninghao.net>
2020-04-03 16:34:12 +0800

    定义上传文件接口

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,1 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+
+/**
+ * 上传文件
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  console.log(request.file);
+  response.sendStatus(200);
+};
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import * as fileController from './file.controller';
+import { authGuard } from '../auth/auth.middleware';
+import { fileInterceptor } from './file.middleware';
 
 const router = express.Router();
+
+/**
+ * 上传文件
+ */
+router.post('/files', authGuard, fileInterceptor, fileController.store);
 
 /**
  * 导出路由
