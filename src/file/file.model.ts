0d02473690df15de0c96fa4e3989283a46f144a8
wanghao <wanghao@ninghao.net>
2020-04-03 17:01:51 +0800

    定义存储文件信息功能

---
+++ src/file/file.model.ts
@@ -0,0 +1,9 @@
+export class FileModel {
+  id?: string;
+  originalname: string;
+  mimetype: string;
+  filename: string;
+  size: number;
+  userId: number;
+  postId?: number;
+}
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,1 +1,19 @@
 import { connection } from '../app/database/mysql';
+import { FileModel } from './file.model';
+
+/**
+ * 存储文件信息
+ */
+export const createFile = async (file: FileModel) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO file
+    SET ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, file);
+
+  // 提供数据
+  return data;
+};
