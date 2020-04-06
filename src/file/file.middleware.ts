4da07423afa4c9fa035be6db32f692b8be88c6bd
wanghao <wanghao@ninghao.net>
2020-04-06 10:47:22 +0800

    读取图像文件附加信息

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import multer from 'multer';
+import Jimp from 'jimp';
 
 /**
  * 创建一个 Multer
@@ -12,3 +13,29 @@ const fileUpload = multer({
  * 文件拦截器
  */
 export const fileInterceptor = fileUpload.single('file');
+
+/**
+ * 文件处理器
+ */
+export const fileProcessor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 文件路径
+  const { path } = request.file;
+
+  let image: Jimp;
+
+  try {
+    // 读取图像文件
+    image = await Jimp.read(path);
+  } catch (error) {
+    return next(error);
+  }
+
+  console.log(image);
+
+  // 下一步
+  next();
+};
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import * as fileController from './file.controller';
 import { authGuard } from '../auth/auth.middleware';
-import { fileInterceptor } from './file.middleware';
+import { fileInterceptor, fileProcessor } from './file.middleware';
 
 const router = express.Router();
 
 /**
  * 上传文件
  */
-router.post('/files', authGuard, fileInterceptor, fileController.store);
+router.post(
+  '/files',
+  authGuard,
+  fileInterceptor,
+  fileProcessor,
+  fileController.store,
+);
 
 /**
  * 文件服务
