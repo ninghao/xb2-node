bad2a477c1ba452cca0d62eb1935adb3fa5614a2
wanghao <wanghao@ninghao.net>
2020-04-08 15:35:38 +0800

    定义按标签名称查找标签的功能

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -17,3 +17,20 @@ export const createTag = async (tag: TagModel) => {
   // 提供数据
   return data as any;
 };
+
+/**
+ * 按名字查找标签
+ */
+export const getTagByName = async (tagName: string) => {
+  // 准备查询
+  const statement = `
+    SELECT id, name FROM tag
+    WHERE name = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, tagName);
+
+  // 提供数据
+  return data[0];
+};
