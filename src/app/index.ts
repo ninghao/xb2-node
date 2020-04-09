65f5c31dd2a93bcfb78dfd2f5c972dac09cfe508
wanghao <wanghao@ninghao.net>
2020-04-09 18:07:47 +0800

    评论模块

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,7 @@ import userRouter from '../user/user.router';
 import authRouter from '../auth/auth.router';
 import fileRouter from '../file/file.router';
 import tagRouter from '../tag/tag.router';
+import commentRouter from '../comment/comment.router';
 import { defaultErrorHandler } from './app.middleware';
 
 /**
@@ -19,7 +20,14 @@ app.use(express.json());
 /**
  * 路由
  */
-app.use(postRouter, userRouter, authRouter, fileRouter, tagRouter);
+app.use(
+  postRouter,
+  userRouter,
+  authRouter,
+  fileRouter,
+  tagRouter,
+  commentRouter,
+);
 
 /**
  * 默认异常处理器
---
+++ src/comment/comment.router.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import * as commentController from './comment.controller';
+
+const router = express.Router();
+
+/**
+ * 导出路由
+ */
+export default router;
