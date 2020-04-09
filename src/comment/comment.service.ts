ff48bf433a49151c36ee34a4070b9d1481bd5e64
wanghao <wanghao@ninghao.net>
2020-04-09 18:28:03 +0800

    定义检查评论是否为回复评论的功能

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -17,3 +17,20 @@ export const createComment = async (comment: CommentModel) => {
   // 提供数据
   return data;
 };
+
+/**
+ * 检查评论是否为回复评论
+ */
+export const isReplyComment = async (commentId: number) => {
+  // 准备查询
+  const statement = `
+    SELECT parentId FROM comment
+    WHERE id = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, commentId);
+
+  // 返回结果
+  return data[0].parentId ? true : false;
+};
