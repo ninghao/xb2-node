5e736532ed3208ac1a35d72704a52abd1f040788
wanghao <wanghao@ninghao.net>
2020-04-09 18:19:22 +0800

    定义发表评论用的接口

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,1 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
+import { createComment } from './comment.service';
+
+/**
+ * 发表评论
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { id: userId } = request.user;
+  const { content, postId } = request.body;
+
+  const comment = {
+    content,
+    postId,
+    userId,
+  };
+
+  try {
+    // 保存评论
+    const data = await createComment(comment);
+
+    // 做出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import * as commentController from './comment.controller';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
+
+/**
+ * 发表评论
+ */
+router.post('/comments', authGuard, commentController.store);
 
 /**
  * 导出路由
