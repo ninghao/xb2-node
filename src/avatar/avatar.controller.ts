8971c1e93f6d7af55ba5155a6be94294c46ae282
wanghao <wanghao@ninghao.net>
2020-04-21 13:45:13 +0800

    定义上传头像接口

--- src/avatar/avatar.controller.ts
+++ src/avatar/avatar.controller.ts
@@ -1,1 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
+
+/**
+ * 上传头像
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  response.sendStatus(200);
+};
--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import * as avatarController from './avatar.controller';
+import { authGuard } from '../auth/auth.middleware';
+import { avatarInterceptor } from './avatar.middleware';
 
 const router = express.Router();
+
+/**
+ * 上传头像
+ */
+router.post('/avatar', authGuard, avatarInterceptor, avatarController.store);
 
 /**
  * 导出路由
