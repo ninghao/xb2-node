6dbc5c00467972316d051b611a0e01849c9207c9
wanghao <wanghao@ninghao.net>
2020-04-21 14:18:09 +0800

    保存头像数据

--- src/avatar/avatar.controller.ts
+++ src/avatar/avatar.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import _ from 'lodash';
+import { createAvatar } from './avatar.service';
 
 /**
  * 上传头像
@@ -8,5 +10,25 @@ export const store = async (
   response: Response,
   next: NextFunction,
 ) => {
-  response.sendStatus(200);
+  // 当前用户 ID
+  const { id: userId } = request.user;
+
+  // 头像文件信息
+  const fileInfo = _.pick(request.file, ['mimetype', 'filename', 'size']);
+
+  // 准备头像数据
+  const avatar = {
+    ...fileInfo,
+    userId,
+  };
+
+  try {
+    // 保存头像数据
+    const data = await createAvatar(avatar);
+
+    // 做出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
 };
