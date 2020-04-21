e96175670cce1181acb2fdc862f15adf07d9a085
wanghao <wanghao@ninghao.net>
2020-04-21 13:37:15 +0800

    创建上传头像中间件

--- src/avatar/avatar.middleware.ts
+++ src/avatar/avatar.middleware.ts
@@ -1,1 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import multer from 'multer';
+import { fileFilter } from '../file/file.middleware';
+
+/**
+ * 文件过滤器
+ */
+const avatarUploadFilter = fileFilter(['image/png', 'image/jpg', 'image/jpeg']);
+
+/**
+ * 创建一个 Multer
+ */
+const avatarUpload = multer({
+  dest: 'uploads/avatar',
+  fileFilter: avatarUploadFilter,
+});
+
+/**
+ * 文件拦截器
+ */
+export const avatarInterceptor = avatarUpload.single('avatar');
