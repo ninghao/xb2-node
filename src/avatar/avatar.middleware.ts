021b07afda10adacb3a5e6b90e5f40379608b488
wanghao <wanghao@ninghao.net>
2020-04-21 14:00:57 +0800

    调整头像的尺寸

--- src/avatar/avatar.middleware.ts
+++ src/avatar/avatar.middleware.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import Jimp from 'jimp';
 import { Request, Response, NextFunction } from 'express';
 import multer from 'multer';
 import { fileFilter } from '../file/file.middleware';
@@ -19,3 +21,43 @@ const avatarUpload = multer({
  * 文件拦截器
  */
 export const avatarInterceptor = avatarUpload.single('avatar');
+
+/**
+ * 头像处理器
+ */
+export const avatarProcessor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备文件信息
+  const { file } = request;
+
+  // 准备文件路径
+  const filePath = path.join(file.destination, 'resized', file.filename);
+
+  // 处理头像文件
+  try {
+    // 读取文件
+    const image = await Jimp.read(file.path);
+
+    // 调整尺寸
+    image
+      .cover(256, 256)
+      .quality(85)
+      .write(`${filePath}-large`);
+    image
+      .cover(128, 128)
+      .quality(85)
+      .write(`${filePath}-medium`);
+    image
+      .cover(64, 64)
+      .quality(85)
+      .write(`${filePath}-small`);
+  } catch (error) {
+    next(error);
+  }
+
+  // 下一步
+  next();
+};
--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import * as avatarController from './avatar.controller';
 import { authGuard } from '../auth/auth.middleware';
-import { avatarInterceptor } from './avatar.middleware';
+import { avatarInterceptor, avatarProcessor } from './avatar.middleware';
 
 const router = express.Router();
 
 /**
  * 上传头像
  */
-router.post('/avatar', authGuard, avatarInterceptor, avatarController.store);
+router.post(
+  '/avatar',
+  authGuard,
+  avatarInterceptor,
+  avatarProcessor,
+  avatarController.store,
+);
 
 /**
  * 导出路由
