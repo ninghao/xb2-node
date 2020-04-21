5f12fb89a86b59c427d69dd6252a3bec12cbbfc1
wanghao <wanghao@ninghao.net>
2020-04-21 13:33:28 +0800

    定义文件过滤器

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -75,6 +75,10 @@ export const defaultErrorHandler = (
       statusCode = 400;
       message = '无法回复这条评论';
       break;
+    case 'FILE_TYPE_NOT_ACCEPT':
+      statusCode = 400;
+      message = '不能上传此类型文件';
+      break;
     default:
       statusCode = 500;
       message = '服务暂时出了点问题 ~~ 🌴';
--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,13 +1,38 @@
 import { Request, Response, NextFunction } from 'express';
-import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import Jimp from 'jimp';
 import { imageResizer } from './file.service';
+
+/**
+ * 文件过滤器
+ */
+export const fileFilter = (fileTypes: Array<string>) => {
+  return (
+    request: Request,
+    file: Express.Multer.File,
+    callback: FileFilterCallback,
+  ) => {
+    // 测试文件类型
+    const allowed = fileTypes.some(type => type === file.mimetype);
+
+    if (allowed) {
+      // 允许上传
+      callback(null, true);
+    } else {
+      // 拒绝上传
+      callback(new Error('FILE_TYPE_NOT_ACCEPT'));
+    }
+  };
+};
+
+const fileUploadFilter = fileFilter(['image/png', 'image/jpg', 'image/jpeg']);
 
 /**
  * 创建一个 Multer
  */
 const fileUpload = multer({
   dest: 'uploads/',
+  fileFilter: fileUploadFilter,
 });
 
 /**
