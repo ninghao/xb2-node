94d4949462da1adb9d9c13352c4a0cc153bb0a99
wanghao <wanghao@ninghao.net>
2020-04-21 14:20:45 +0800

    定义按用户 ID 查找头像的功能

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -17,3 +17,23 @@ export const createAvatar = async (avatar: AvatarModel) => {
   // 提供数据
   return data;
 };
+
+/**
+ * 按用户 ID 查找 头像
+ */
+export const findAvatarByUserId = async (userId: number) => {
+  // 准备查询
+  const statement = `
+    SELECT *
+    FROM avatar
+    WHERE userId = ?
+    ORDER BY avatar.id DESC
+    LIMIT 1
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, userId);
+
+  // 提供数据
+  return data[0];
+};
