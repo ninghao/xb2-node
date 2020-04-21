9d3f28d2e22e953a6f909c580c454f6ff5f5e998
wanghao <wanghao@ninghao.net>
2020-04-21 14:35:00 +0800

    在内容中标记用户是否上传过头像

--- src/post/post.provider.ts
+++ src/post/post.provider.ts
@@ -5,12 +5,15 @@ export const sqlFragment = {
   user: `
     JSON_OBJECT(
       'id', user.id,
-      'name', user.name
+      'name', user.name,
+      'avatar', IF(COUNT(avatar.id), 1, null)
     ) as user
   `,
   leftJoinUser: `
     LEFT JOIN user
-    ON user.id = post.userId
+      ON user.id = post.userId
+    LEFT JOIN avatar
+      ON user.id = avatar.userId
   `,
   totalComments: `
     (
