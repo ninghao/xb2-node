43cfc9232c4c98ff837cf7eb246dcb5609ae1dff
wanghao <wanghao@ninghao.net>
2020-04-22 14:36:42 +0800

    定义点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,1 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
+import { createUserLikePost } from './like.service';
+
+/**
+ * 点赞内容
+ */
+export const storeUserLikePost = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { postId } = request.params;
+  const { id: userId } = request.user;
+
+  // 点赞内容
+  try {
+    const data = await createUserLikePost(userId, parseInt(postId, 10));
+
+    // 做出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import * as likeController from './like.controller';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
+
+/**
+ * 点赞内容
+ */
+router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePost);
 
 /**
  * 导出路由
