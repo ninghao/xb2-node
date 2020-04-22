fbb5a8b2677c1380d3524d02566814d579fae92d
wanghao <wanghao@ninghao.net>
2020-04-22 14:32:54 +0800

    定义保存点赞内容的功能

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,1 +1,19 @@
 import { connection } from '../app/database/mysql';
+
+/**
+ * 保存用户点赞内容
+ */
+export const createUserLikePost = async (userId: number, postId: number) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO
+      user_like_post (userId, postId)
+    VALUES (?, ?)
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, [userId, postId]);
+
+  // 提供数据
+  return data;
+};
