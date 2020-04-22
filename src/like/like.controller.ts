8b112284eb7af27ab55af2212369182367b624e5
wanghao <wanghao@ninghao.net>
2020-04-22 15:18:50 +0800

    定义取消点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { createUserLikePost } from './like.service';
+import { createUserLikePost, deleteUserLikePost } from './like.service';
 
 /**
  * 点赞内容
@@ -19,6 +19,29 @@ export const storeUserLikePost = async (
 
     // 做出响应
     response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * 取消点赞内容
+ */
+export const destroyUserLikePost = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { postId } = request.params;
+  const { id: userId } = request.user;
+
+  // 取消点赞内容
+  try {
+    const data = await deleteUserLikePost(userId, parseInt(postId, 10));
+
+    // 做出响应
+    response.send(data);
   } catch (error) {
     next(error);
   }
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -10,6 +10,15 @@ const router = express.Router();
 router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePost);
 
 /**
+ * 取消点赞内容
+ */
+router.delete(
+  '/posts/:postId/like',
+  authGuard,
+  likeController.destroyUserLikePost,
+);
+
+/**
  * 导出路由
  */
 export default router;
