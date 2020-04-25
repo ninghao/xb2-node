c7dcecdbee360710fa4e101a065fbbd712fb963f
wanghao <wanghao@ninghao.net>
2020-04-25 15:45:43 +0800

    获取内容的评论列表

--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -8,11 +8,23 @@ export const filter = async (
   response: Response,
   next: NextFunction,
 ) => {
+  // 解构查询符
+  const { post, user, action } = request.query;
+
   // 默认的过滤
   request.filter = {
     name: 'default',
     sql: 'comment.parentId IS NULL',
   };
+
+  // 内容的评论
+  if (post && !user && !action) {
+    request.filter = {
+      name: 'postComments',
+      sql: 'comment.parentId IS NULL AND comment.postId = ?',
+      param: post,
+    };
+  }
 
   // 下一步
   next();
