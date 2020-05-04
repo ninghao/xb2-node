df0c47d8ccb46c50305fcf33bc88b105411d2665
wanghao <wanghao@ninghao.net>
2020-05-04 10:56:23 +0800

    测试创建用户接口

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -33,3 +33,45 @@ afterAll(async () => {
   // 断开数据服务连接
   connection.end();
 });
+
+/**
+ * 创建用户
+ */
+describe('测试创建用户接口', () => {
+  test('创建用户时必须提供用户名', async () => {
+    // 请求接口
+    const response = await request(app)
+      .post('/users')
+      .send({ password: testUser.password });
+
+    // 做出断言
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: '请提供用户名' });
+  });
+
+  test('创建用户时必须提供密码', async () => {
+    // 请求接口
+    const response = await request(app)
+      .post('/users')
+      .send({ name: testUser.name });
+
+    // 做出断言
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: '请提供用户密码' });
+  });
+
+  test('成功创建用户以后，响应状态码应该是 201', async () => {
+    // 请求接口
+    const response = await request(app)
+      .post('/users')
+      .send(testUser);
+
+    // 设置创建的测试用户
+    testUserCreated = await getUserById(response.body.insertId, {
+      password: true,
+    });
+
+    // 做出断言
+    expect(response.status).toBe(201);
+  });
+});
