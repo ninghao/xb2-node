fa9bfbe323dc60bab0e35482237e483d2fc96b24
wanghao <wanghao@ninghao.net>
2020-05-04 10:41:57 +0800

    创建接口测试

--- src/app/app.test.ts
+++ src/app/app.test.ts
@@ -1,3 +1,6 @@
+import request from 'supertest';
+import app from '../app';
+import { connection } from './database/mysql';
 import { greet } from './playground/demo';
 
 /**
@@ -11,5 +14,35 @@ describe('演示单元测试', () => {
 
     // 断言
     expect(greeting).toBe('你好，王皓');
+  });
+});
+
+/**
+ * 测试接口
+ */
+describe('演示接口测试', () => {
+  afterAll(async () => {
+    // 断开数据服务连接
+    connection.end();
+  });
+
+  test('测试 GET /', async () => {
+    // 请求接口
+    const response = await request(app).get('/');
+
+    // 做出断言
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ title: '小白兔的开发之路' });
+  });
+
+  test('测试 POST /echo', async () => {
+    // 请求接口
+    const response = await request(app)
+      .post('/echo')
+      .send({ message: '你好 ~' });
+
+    // 做出断言
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual({ message: '你好 ~' });
   });
 });
