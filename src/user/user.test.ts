99ad121654d4ff90e032137abb55ac9cd659f556
wanghao <wanghao@ninghao.net>
2020-05-04 11:03:33 +0800

    测试用户帐户接口

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -75,3 +75,30 @@ describe('测试创建用户接口', () => {
     expect(response.status).toBe(201);
   });
 });
+
+/**
+ * 用户帐户
+ */
+describe('测试用户帐户接口', () => {
+  test('响应里应该包含指定的属性', async () => {
+    // 请求接口
+    const response = await request(app).get(`/users/${testUserCreated.id}`);
+
+    // 做出断言
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe(testUser.name);
+    expect(response.body).toMatchObject({
+      id: expect.any(Number),
+      name: expect.any(String),
+      avatar: null,
+    });
+  });
+
+  test('当用户不存在时，响应的状态码是 404', async () => {
+    // 请求接口
+    const response = await request(app).get('/users/-1');
+
+    // 做出断言
+    expect(response.status).toBe(404);
+  });
+});
